b6988a26f1f05395aae9399c1292248188ff1711
Carl Timmer <timmer@jlab.org>
2022-02-11 14:34:13 -0500

    put in htonll & ntohll

--- simulation/ejfat_assemble_ersap.hpp
+++ simulation/ejfat_assemble_ersap.hpp
@@ -44,6 +44,12 @@
 #define btoa(x) ((x)?"true":"false")
 
 
+#ifdef __linux__
+    #define htonll(x) ((1==htonl(1)) ? (x) : (((uint64_t)htonl((x) & 0xFFFFFFFFUL)) << 32) | htonl((uint32_t)((x) >> 32)))
+    #define ntohll(x) ((1==ntohl(1)) ? (x) : (((uint64_t)ntohl((x) & 0xFFFFFFFFUL)) << 32) | ntohl((uint32_t)((x) >> 32)))
+#endif
+
+
 #ifndef _BYTESWAP_H
 #define _BYTESWAP_H
 
