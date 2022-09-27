5746af8a25db86f18409b8e32f157e89d631cd8b
Carl Timmer <timmer@jlab.org>
2022-09-27 12:52:42 -0400

    again

--- simulation/ejfat_assemble_ersap.hpp
+++ simulation/ejfat_assemble_ersap.hpp
@@ -767,7 +767,8 @@ fprintf(stderr, "getPacketizedBuffer: buf too small? nBytes = %d, remainingLen =
                             printf("Backed up a whole tick, got %" PRIu64 ", prev = %" PRIu64 "\n", packetTick, prevTick);
                         }
                         else if (packetTick != expectedTick) {
-                            printf("Packet != expected tick, got %" PRIu64 ", ex = %" PRIu64 "\n", packetTick, expectedTick);
+                            printf("Packet != expected tick, got %" PRIu64 ", ex = %" PRIu64 ", prev = %" PRIu64 "\n",
+                                   packetTick, expectedTick, prevTick);
                         }
                     }
 
