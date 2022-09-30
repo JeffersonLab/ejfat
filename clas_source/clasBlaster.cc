22442399057cd79f7cca2f9420d846131f732297
Carl Timmer <timmer@jlab.org>
2022-09-30 15:08:50 -0600

    remove printout

--- clas_source/clasBlaster.cc
+++ clas_source/clasBlaster.cc
@@ -915,7 +915,7 @@ int main(int argc, char **argv) {
 //fprintf(stderr, "packetBlaster: read next event\n");
         }
         else {
-            fprintf(stderr, "again\n");
+//            fprintf(stderr, "again\n");
             reader.gotoEvent(0);
             reader.read(event);
         }
