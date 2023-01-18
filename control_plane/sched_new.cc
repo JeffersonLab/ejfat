4cc568ad94b8ee58fd113950b2edcca2f9e5b2d4
Carl Timmer <timmer@jlab.org>
2023-01-18 16:20:41 -0500

    fix bug

--- control_plane/sched_new.cc
+++ control_plane/sched_new.cc
@@ -174,7 +174,7 @@ int main (int argc, char *argv[])
         std::this_thread::sleep_for(std::chrono::seconds(2));
 
         // This needs to be called each loop since it gets a COPY of the current data (for thread safety)
-        std::shared_ptr<std::unordered_map<std::string, BackEnd>> pDataMap = service->getBackEnds();
+        std::shared_ptr<std::unordered_map<std::string, BackEnd>> pDataMap = service.getBackEnds();
         size_t num_bes = pDataMap->size(); //number of backends giving feed back this reporting interval
 
         // Loop over all backends
