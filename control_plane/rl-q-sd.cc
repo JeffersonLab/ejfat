285ed3babb7835c6cbca483f31455ca340a82b84
Michael Goodrich <goodrich@jlab.org>
2023-03-01 17:46:31 -0500

    Correlate Reward and Schedule Density, fix some RL bugs, remove some garbage

--- control_plane/rl-q-sd.cc
+++ control_plane/rl-q-sd.cc
@@ -40,7 +40,7 @@ void   Usage(void)
         -d debug mode \n\
         -h help \n\n";
         cout<<usage_str;
-        cout<<"Required: -i -p\n";
+        cout<<"Required: -n -f\n";
 }
 
 template<class X>
@@ -67,13 +67,13 @@ size_t smpl_wghtd(X w)
     vector<double> t;
     t.push_back(N*w[0]);
     for(size_t i=1;i<s;i++) t.push_back(N*w[i]+t[i-1]);
-cout << "softmax t = "; print(t);            
+cout << "smpl_wghtd t = "; print(t);            
     //now get random uniform in [1,N]:
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine g (seed);
     std::uniform_int_distribution<int> u(1,N);
     size_t r = u(g);
-cout << "softmax r = " << r << '\n';            
+cout << "smpl_wghtd r = " << r << '\n';            
     //now index into weight array
     for(size_t i=0;i<s;i++) if(r<=t[i]) return i; //fix this hack
     return s;
@@ -126,9 +126,9 @@ int main (int argc, char *argv[])
     double beta_1 = 1; //  temperature
 
     // R vector with the first value set at 0.5
-    vector<double> R; R.resize(num_hsts); for(uint16_t h=0;h<num_hsts;h++) R[h] = 0.5+h*0.2;
+    vector<double> R; R.resize(num_hsts); //for(uint16_t h=0;h<num_hsts;h++) R[h] = 0.5+h*0.2;
     // Q vector with the first value set at 0.5
-    vector<double> Q; Q.resize(num_hsts); for(uint16_t h=0;h<num_hsts;h++) Q[h] = 0.5+h*0.1;
+    vector<double> Q; Q.resize(num_hsts); for(uint16_t h=0;h<num_hsts;h++) Q[h] = 0;
     ifstream hst_fdbk;
  //   std::discrete_distribution<uint16_t> d;
      // random device class instance, source of 'true' randomness for initializing random seed
@@ -143,11 +143,21 @@ int main (int argc, char *argv[])
     //Num  Successes
     vector<uint32_t> NS; NS.resize(num_hsts); for(uint16_t h=0;h<num_hsts;h++) NS[h] = 0;
 
-    uint32_t trl=0;   // loop counter
+    uint32_t trl=1;   // loop counter
+
+    // set initial schedule Density (SD) ////////////
+    vector<double> SD;   SD.resize(num_hsts);
+    softmax(Q, SD, num_hsts, beta_1);
+    ///////// set initial Reward
+    {
+        size_t s = R.size();
+        for(size_t i=0;i<s;i++) R[i] += d(gen);
+    }      
 
     do {
         // Monitor feedback from nodes and adjust scheduling density
 
+/*****************************************
         //sleep(1);
         //read host feedback: an array of health metrics
         hst_fdbk.open(hst_fdbk_t,std::ios::in);
@@ -162,97 +172,31 @@ if(passedD) cout << "read " << x << " from feedback file\n";
             R.push_back(R[i++]+x);
         }
         hst_fdbk.close();
-///////////// RW test /////////////////////////
-        {
-            size_t s = R.size();
-            for(size_t i=0;i<s;i++) R[i] += d(gen);
-        }      
+*****************************************/
 ///////////////////////////////////////////////
         //theta <- rep(NA, times = N_trials)
 //        d.param(Q.begin(),Q.end()); //update action selection probability weightings from Q
 
-        vector<double> t; t.resize(num_hsts);
+if(passedD) cout << "SD = "; print(SD);            
 if(passedD) cout << "Q = "; print(Q);            
-        softmax(Q, t, num_hsts, beta_1);
-if(passedD) cout << "t = "; print(t);            
-        uint16_t action = smpl_wghtd(t);  //d();  //which.max(R[t,]) #
+if(passedD) cout << "R = "; print(R);            
+        uint16_t action = smpl_wghtd(SD);  //d();  //which.max(R[t,]) #
 if(passedD) cout << "action = " << action << "\n";            
         Q[action] += alpha * (R[action] - Q[action]);
+        softmax(Q, SD, num_hsts, beta_1);
 if(passedD) cout << "new Q = "; print(Q);            
-/*
-        nonactions_t <- (1:num_hsts)[-action[t]]
-        Q[t + 1, nonactions_t] <- Q[t, nonactions_t]
-        SM <- apply(Q, 1, softmax, temp=1)
-        plot(SM[1,],type='l',col=clrs[1],ylim = c(min(SM),max(SM)))
-        for (a in 2:num_hsts) {
-          lines(SM[a,],col=clrs[a])
-        }
-*/
         //what are the betting odds after each trial? (LaPlace's Rule of Succession)
 //        BO <- matrix(nrow = N_trials, ncol = num_hsts)
 //        NS <- vector(length = num_hsts)
-          NS[action]++;
+        NS[action]++;
 if(passedD) cout << "NS = "; print(NS);            
-          for(size_t i=0;i<num_hsts;i++)  BO[i] = double(NS[i]+1)/double(trl+2);
-            //cout << "BO["<<i<<"] = "<<BO[i]<<" for NS["<<i<<"] = "<<NS[i]<<" and trl = "<<trl<<'\n';
-          //}
+        for(size_t i=0;i<num_hsts;i++)  BO[i] = double(NS[i]+1)/double(trl+2);
+	double BOsum = accumulate(BO.begin(),BO.end(),0.0);
+        for(size_t i=0;i<num_hsts;i++)  BO[i] /= BOsum;   //normalize
 if(passedD) cout << "BO = "; print(BO);            
-          trl++;
+        R[action] *= SD[action]; // need to add some noise
+        trl++;
     } while(1);
 
     return 0;
 }
-/*
-// discrete_distribution constructor
-#include <iostream>
-#include <chrono>
-#include <random>
-#include <functional>
-#include <array>
-
-int main()
-{
-  // construct a trivial random generator engine from a time-based seed:
-  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-  std::default_random_engine generator (seed);
-
-  std::array<double,4> init = {1.0,2.0,3.0,4.0};
-
-  std::discrete_distribution<int> first;
-  std::discrete_distribution<int> second (init.begin(),init.end());
-  std::discrete_distribution<int> third {0.1,0.2,0.3,0.4};
-  std::discrete_distribution<int> fourth (4,0.0,40.0,std::bind2nd(std::plus<double>(),5.0));
-  std::discrete_distribution<int> fifth (fourth.param());
-
-  // display probabilities:
-  std::cout << "displaying probabilities:";
-  std::cout << std::endl << "first : ";
-  for (double x:first.probabilities()) std::cout << x << " ";
-  std::cout << std::endl << "second: ";
-  for (double x:second.probabilities()) std::cout << x << " ";
-  std::cout << std::endl << "third : ";
-  for (double x:third.probabilities()) std::cout << x << " ";
-  std::cout << std::endl << "fourth: ";
-  for (double x:fourth.probabilities()) std::cout << x << " ";
-  std::cout << std::endl << "fifth : ";
-  for (double x:fifth.probabilities()) std::cout << x << " ";
-  std::cout << std::endl;
-
-  return 0;
-}
-
-#include <random>
-
-int main()
-{
-  std::default_random_engine generator;
-  std::discrete_distribution<int> d1 {10.0,20.0,20.0,25.0,25.0};
-  std::discrete_distribution<int> d2 (d1.param());
-
-  // print two independent values:
-  std::cout << d1(generator) << std::endl;
-  std::cout << d2(generator) << std::endl;
-
-  return 0;
-}
-*/
