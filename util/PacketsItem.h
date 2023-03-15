bd34497073f80b9c762877b9d7cf44f21507781a
Carl Timmer <timmer@jlab.org>
2023-03-15 10:37:40 -0400

    again

--- util/PacketsItem.h
+++ util/PacketsItem.h
@@ -30,6 +30,9 @@
     #ifndef _GNU_SOURCE
         #define _GNU_SOURCE
     #endif
+
+    #define htonll(x) ((1==htonl(1)) ? (x) : (((uint64_t)htonl((x) & 0xFFFFFFFFUL)) << 32) | htonl((uint32_t)((x) >> 32)))
+    #define ntohll(x) ((1==ntohl(1)) ? (x) : (((uint64_t)ntohl((x) & 0xFFFFFFFFUL)) << 32) | ntohl((uint32_t)((x) >> 32)))
 #endif
 
 
