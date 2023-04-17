e60950bc5055f2d16dbce7c5ac5cc70a86740c46
Carl Timmer <timmer@jlab.org>
2023-04-17 17:47:32 -0400

    clean up and update

--- simulation/packetBlasteeTcp.cc
+++ simulation/packetBlasteeTcp.cc
@@ -16,14 +16,14 @@
 
 #include <cstdlib>
 #include <iostream>
-#include <time.h>
+#include <ctime>
 #include <thread>
 #include <cmath>
 #include <chrono>
 #include <atomic>
 #include <algorithm>
 #include <cstring>
-#include <errno.h>
+#include <cerrno>
 #include <cinttypes>
 
 #include "ejfat_assemble_ersap.hpp"
@@ -56,16 +56,13 @@ using namespace ejfat;
  */
 static void printHelp(char *programName) {
     fprintf(stderr,
-            "\nusage: %s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n",
+            "\nusage: %s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n",
             programName,
             "        [-h] [-v] [-ip6]",
             "        [-a <listening IP address (defaults to INADDR_ANY)>]",
             "        [-p <listening UDP port>]",
             "        [-b <internal buffer byte sizez>]",
             "        [-r <UDP receive buffer byte size>]",
-            "        [-fifo (Use SCHED_FIFO realtime scheduler for process - linux)]",
-            "        [-rr (Use SCHED_RR realtime scheduler for process - linux)]",
-            "        [-pri <realtime process priority, default = max>]",
             "        [-f <file for stats>]",
             "        [-s_host <TCP server host>]",
             "        [-s_port <TCP server port>]",
@@ -87,11 +84,8 @@ static void printHelp(char *programName) {
  * @param tickPrescale  expected increase in tick with each incoming buffer.
  * @param cores         array of core ids on which to run assembly thread.
  * @param port          filled with UDP port to listen on.
- * @param rtPriority    filled with realtime priority.
  * @param debug         filled with debug flag.
  * @param useIPv6       filled with use IP version 6 flag.
- * @param useFIFO       filled with use FIFO realtime scheduler flag.
- * @param useRR         filled with se RR (round robin) realtime scheduler flag.
  * @param listenAddr    filled with IP address to listen on.
  * @param filename      filled with name of file in which to write stats.
  * @param server        IP address of TCP server to send buffers to.
@@ -100,8 +94,8 @@ static void printHelp(char *programName) {
  */
 static void parseArgs(int argc, char **argv,
                       int* bufSize, int *recvBufSize, int *tickPrescale,
-                      int *cores, uint16_t* port, int *rtPriority,
-                      bool *debug, bool *useIPv6, bool *useFIFO, bool *useRR,
+                      int *cores, uint16_t* port,
+                      bool *debug, bool *useIPv6,
                       char *listenAddr, char *filename,
                       char *server, uint16_t *serverPort, char *serverIF) {
 
@@ -113,9 +107,6 @@ static void parseArgs(int argc, char **argv,
             {             {"tpre",  1, NULL, 1},
                           {"ip6",  0, NULL, 2},
                           {"cores",  1, NULL, 3},
-                          {"fifo",  0, NULL, 4},
-                          {"rr",  0, NULL, 5},
-                          {"pri",  1, NULL, 6},
                           {"s_host",  1, NULL, 7},
                           {"s_port",  1, NULL, 8},
                           {"s_if",  1, NULL, 9},
@@ -263,35 +254,6 @@ static void parseArgs(int argc, char **argv,
                 }
                 break;
 
-            case 4:
-                // use FIFO realtime scheduler
-                if (*useRR) {
-                    fprintf(stderr, "Cannot specify both FIFO and RR\n");
-                }
-                *useFIFO = true;
-                break;
-
-            case 5:
-                // use RR (round robin) realtime scheduler
-                if (*useFIFO) {
-                    fprintf(stderr, "Cannot specify both FIFO and RR\n");
-                }
-                *useRR = true;
-                break;
-
-            case 6:
-                // Realtime priority
-                i_tmp = (int) strtol(optarg, nullptr, 0);
-                if (i_tmp >= 1) {
-                    *rtPriority = i_tmp;
-                }
-                else {
-                    fprintf(stderr, "Invalid argument to -pri, pri >= 1\n\n");
-                    printHelp(argv[0]);
-                    exit(-1);
-                }
-                break;
-
             case 7:
                 // TCP server host
                 if (strlen(optarg) > 100 || strlen(optarg) < 1) {
@@ -349,12 +311,13 @@ static void parseArgs(int argc, char **argv,
 
 
 
-
 // Statistics
 static volatile uint64_t totalBytes=0, totalPackets=0;
 static volatile int cpu=-1;
-static std::atomic<uint32_t> droppedPackets;
-static std::atomic<uint32_t> droppedTicks;
+static int64_t discardedPackets, discardedBytes;
+static int32_t discardedTicks;
+
+
 typedef struct threadStruct_t {
     char filename[101];
 } threadStruct;
@@ -363,16 +326,16 @@ typedef struct threadStruct_t {
 // Thread to send to print out rates
 static void *thread(void *arg) {
 
-    uint64_t packetCount, byteCount;
-    uint64_t prevTotalPackets, prevTotalBytes;
-    uint64_t currTotalPackets, currTotalBytes;
+    int64_t packetCount, byteCount;
+    int64_t prevTotalPackets, prevTotalBytes;
+    int64_t currTotalPackets, currTotalBytes;
     // Ignore first rate calculation as it's most likely a bad value
     bool skipFirst = true;
 
 
     // File writing stuff
     bool writeToFile = false;
-    threadStruct *targ = static_cast<threadStruct *>(arg);
+    auto *targ = static_cast<threadStruct *>(arg);
     char *filename = targ->filename;
     FILE *fp;
     if (strlen(filename) > 0) {
@@ -382,7 +345,7 @@ static void *thread(void *arg) {
         // validate file open for writing
         if (!fp) {
             fprintf(stderr, "file open failed: %s\n", strerror(errno));
-            return (NULL);
+            return (nullptr);
         }
 
         // Write column headers
@@ -391,7 +354,7 @@ static void *thread(void *arg) {
 
 
     double pktRate, pktAvgRate, dataRate, dataAvgRate, totalRate, totalAvgRate;
-    int64_t totalT = 0, time, droppedPkts, totalDroppedPkts = 0, droppedTiks, totalDroppedTiks = 0;
+    int64_t totalT = 0, time, discardedPkts, totalDiscardedPkts = 0, discardedTiks, totalDiscardedTiks = 0;
     struct timespec t1, t2, firstT;
 
     // Get the current time
@@ -436,29 +399,29 @@ static void *thread(void *arg) {
         }
 
         // Dropped stuff rates
-        droppedPkts = droppedPackets;
-        droppedPackets.store(0);
-        totalDroppedPkts += droppedPkts;
+        discardedPkts = discardedPackets;
+        discardedPackets = 0;
+        totalDiscardedPkts += discardedPkts;
 
-        droppedTiks = droppedTicks;
-        droppedTicks.store(0);
-        totalDroppedTiks += droppedTiks;
+        discardedTiks = discardedTicks;
+        discardedTicks = 0;
+        totalDiscardedTiks += discardedTiks;
 
         pktRate = 1000000.0 * ((double) packetCount) / time;
         pktAvgRate = 1000000.0 * ((double) currTotalPackets) / totalT;
-        if (packetCount == 0 && droppedPkts == 0) {
-            printf(" Packets:  %3.4g Hz,  %3.4g Avg, dropped pkts = 0?/everything? ", pktRate, pktAvgRate);
+        if (packetCount == 0 && discardedPkts == 0) {
+            printf(" Packets:  %3.4g Hz,  %3.4g Avg, dumped pkts = 0?/everything? ", pktRate, pktAvgRate);
         }
         else {
-            printf(" Packets:  %3.4g Hz,  %3.4g Avg, dropped pkts = %" PRId64 " ", pktRate, pktAvgRate, droppedPkts);
+            printf(" Packets:  %3.4g Hz,  %3.4g Avg, dumped pkts = %" PRId64 ",  total %" PRId64 " ",
+                   pktRate, pktAvgRate, discardedPkts, totalDiscardedPkts);
         }
-        printf(": Dropped Ticks = %" PRId64 ", total = %" PRId64 "\n", droppedTiks, totalDroppedTiks);
+        printf(": Dumped Ticks = %" PRId64 ", total = %" PRId64 "\n", discardedTiks, totalDiscardedTiks);
 
         // Actual Data rates (no header info)
         dataRate = ((double) byteCount) / time;
         dataAvgRate = ((double) currTotalBytes) / totalT;
-        printf(" Data:     %3.4g MB/s,  %3.4g Avg, cpu %d, dropped pkts %" PRId64 ", total %" PRId64 "\n",
-               dataRate, dataAvgRate, cpu, droppedPkts, totalDroppedPkts);
+        printf(" Data:     %3.4g MB/s,  %3.4g Avg, cpu %d\n", dataRate, dataAvgRate, cpu);
 
         totalRate = ((double) (byteCount + HEADER_BYTES*packetCount)) / time;
         totalAvgRate = ((double) (currTotalBytes + HEADER_BYTES*currTotalPackets)) / totalT;
@@ -466,7 +429,7 @@ static void *thread(void *arg) {
 
         if (writeToFile) {
             fprintf(fp, "%" PRId64 ",%d,%d,%" PRId64 ",%" PRId64 ",%d\n", totalT/1000000, (int)(pktRate/1000), (int)(dataRate),
-                    droppedPkts, totalDroppedPkts, cpu);
+                    discardedPkts, totalDiscardedPkts, cpu);
             fflush(fp);
         }
 
@@ -474,9 +437,8 @@ static void *thread(void *arg) {
     }
 
     fclose(fp);
-    return (NULL);
+    return (nullptr);
 }
-
 
 
 
@@ -489,14 +451,11 @@ int main(int argc, char **argv) {
     int bufSize = 1020000;
     int recvBufSize = 0;
     int tickPrescale = 1;
-    int rtPriority = 0;
-    uint16_t port = 7777;
+    uint16_t port = 17750;
     uint16_t serverPort = 8888;
     int cores[10];
     bool debug = false;
     bool useIPv6 = false;
-    bool useFIFO = false;
-    bool useRR = false;
 
     char listeningAddr[16];
     memset(listeningAddr, 0, 16);
@@ -512,8 +471,8 @@ int main(int argc, char **argv) {
         cores[i] = -1;
     }
 
-    parseArgs(argc, argv, &bufSize, &recvBufSize, &tickPrescale, cores, &port, &rtPriority, &debug,
-              &useIPv6, &useFIFO, &useRR, listeningAddr, filename, server, &serverPort, interface);
+    parseArgs(argc, argv, &bufSize, &recvBufSize, &tickPrescale, cores, &port, &debug,
+              &useIPv6, listeningAddr, filename, server, &serverPort, interface);
 
 
     std::cerr << "Tick prescale = " << tickPrescale << "\n";
@@ -546,66 +505,6 @@ int main(int argc, char **argv) {
         if (rc != 0) {
             std::cerr << "Error calling pthread_setaffinity_np: " << rc << std::endl;
         }
-    }
-
-    if (useFIFO || useRR) {
-        // Using the actual pid will set priority of main thd.
-        // Using 0 will set priority of calling thd.
-        pid_t myPid = getpid();
-        // myPid = 0;
-
-        struct sched_param param;
-        int policy = useFIFO ? SCHED_FIFO : SCHED_RR;
-
-        // Set process to correct priority for given scheduler
-        int priMax = sched_get_priority_max(policy);
-        int priMin = sched_get_priority_min(policy);
-
-        // If error
-        if (priMax == -1 || priMin == -1) {
-            perror("Error reading priority");
-            exit(EXIT_FAILURE);
-        }
-
-        if (rtPriority < 1 || rtPriority > priMax) {
-            rtPriority = priMax;
-        }
-        else if (rtPriority < priMin) {
-            rtPriority = priMin;
-        }
-
-        // Current scheduler policy
-        int currPolicy = sched_getscheduler(myPid);
-        if (currPolicy < 0) {
-            perror("Error reading policy");
-            exit(EXIT_FAILURE);
-        }
-        std::cerr << "Current Scheduling Policy: " << currPolicy <<
-                     " (RR = " << SCHED_RR << ", FIFO = " << SCHED_FIFO <<
-                     ", OTHER = " << SCHED_OTHER << ")" << std::endl;
-
-        // Set new scheduler policy
-        std::cerr << "Setting Scheduling Policy to: " << policy << ", pri = " << rtPriority << std::endl;
-        param.sched_priority = rtPriority;
-        int errr = sched_setscheduler(myPid, policy, &param);
-        if (errr < 0) {
-            perror("Error setting scheduler policy");
-            exit(EXIT_FAILURE);
-        }
-
-        errr = sched_getparam(myPid, &param);
-        if (errr < 0) {
-            perror("Error getting priority");
-            exit(EXIT_FAILURE);
-        }
-
-        currPolicy = sched_getscheduler(myPid);
-        if (currPolicy < 0) {
-            perror("Error reading policy");
-            exit(EXIT_FAILURE);
-        }
-
-        std::cerr << "New Scheduling Policy: " << currPolicy << ", pri = " <<  param.sched_priority <<std::endl;
     }
 
 #endif
@@ -698,7 +597,7 @@ int main(int argc, char **argv) {
     }
 
     // Start thread to do rate printout
-    threadStruct *targ = (threadStruct *)malloc(sizeof(threadStruct));
+    auto *targ = (threadStruct *)malloc(sizeof(threadStruct));
     if (targ == nullptr) {
         fprintf(stderr, "out of mem\n");
         return -1;
@@ -709,28 +608,20 @@ int main(int argc, char **argv) {
     }
 
     pthread_t thd;
-    int status = pthread_create(&thd, NULL, thread, (void *) targ);
+    int status = pthread_create(&thd, nullptr, thread, (void *) targ);
     if (status != 0) {
         fprintf(stderr, "\n ******* error creating thread\n\n");
         return -1;
     }
 
-     // If bufSize gets too big, it exceeds stack limits, so lets malloc it!
+    // If bufSize gets too big, it exceeds stack limits, so lets malloc it!
     char *dataBuf = (char *) malloc(bufSize);
-    if (dataBuf == NULL) {
+    if (dataBuf == nullptr) {
         fprintf(stderr, "cannot allocate internal buffer memory of %d bytes\n", bufSize);
         return -1;
     }
 
     fprintf(stderr, "Internal buffer size = %d bytes\n", bufSize);
-
-    /*
-     * Map to hold out-of-order packets.
-     * map key = sequence/offset from incoming packet
-     * map value = tuple of (buffer of packet data which was allocated), (bufSize in bytes),
-     * (is last packet), (is first packet).
-     */
-    std::map<uint32_t, std::tuple<char *, uint32_t, bool, bool>> outOfOrderPackets;
 
     // Track cpu by calling sched_getcpu roughly once per sec
     int cpuLoops = 50000;
@@ -738,8 +629,8 @@ int main(int argc, char **argv) {
 
     // Statistics
     std::shared_ptr<packetRecvStats> stats = std::make_shared<packetRecvStats>();
-    droppedTicks.store(0);
-    droppedPackets.store(0);
+    discardedTicks = 0;
+    discardedPackets = 0;
 
     int tcpSocket = 0;
 
@@ -763,9 +654,8 @@ int main(int argc, char **argv) {
         uint64_t diff, prevTick = tick;
 
         // Fill with data
-        nBytes = getCompletePacketizedBuffer(dataBuf, bufSize, udpSocket,
-                                             debug, &tick, &dataId, stats,
-                                             tickPrescale, outOfOrderPackets);
+        nBytes = getCompletePacketizedBufferNew(dataBuf, bufSize, udpSocket,
+                                             debug, &tick, &dataId, stats, tickPrescale);
         if (nBytes < 0) {
             if (nBytes == BUF_TOO_SMALL) {
                 fprintf(stderr, "Receiving buffer is too small (%d)\n", bufSize);
@@ -799,9 +689,10 @@ int main(int argc, char **argv) {
         totalBytes   += nBytes;
         totalPackets += stats->acceptedPackets;
 
-        // atomic
-        droppedTicks   += stats->droppedBuffers;
-        droppedPackets += stats->droppedPackets;
+        // stats
+        discardedTicks   += stats->discardedBuffers;
+        discardedBytes   += stats->discardedBytes;
+        discardedPackets += stats->discardedPackets;
 
         // The tick returned is what was just built.
         // Now give it the next expected tick.
