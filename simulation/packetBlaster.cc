f851a98fd6777300d082a1a8160a56e3fd37e55f
Carl Timmer <timmer@jlab.org>
2023-04-17 16:01:02 -0400

    cleanup

--- simulation/packetBlaster.cc
+++ simulation/packetBlaster.cc
@@ -14,18 +14,19 @@
  * to an ejfat router (FPGA-based or simulated) which then passes it
  * to the receiving program packetBlastee.cc.
  * </p>
- * Look on my Mac at /Users/timmer/DataGraphs/NumaNodes.xlsx.
+ * Look on Carl Timmer's Mac at /Users/timmer/DataGraphs/NumaNodes.xlsx in order see results
+ * of a study of the ejfat nodes at Jefferson Lab, 100Gb network.
  * To produce events at roughly 2.9GB/s, use arg "-cores 60" where 60 can just as
  * easily be 0-63. To produce at roughly 3.4 GB/s, use cores 64-79, 88-127.
  * To produce at 4.7 GB/s, use cores 80-87. (Notices cores # start at 0).
  * The receiver, packetBlasteeNew2 will be able to receive all data sent at
- * 2.9GB/s, any more than that and it starts dropping packets.
+ * 3 GB/s, any more than that and it starts dropping packets.
  */
 
 
 #include <unistd.h>
 #include <cstdlib>
-#include <time.h>
+#include <ctime>
 #include <cmath>
 #include <thread>
 #include <pthread.h>
