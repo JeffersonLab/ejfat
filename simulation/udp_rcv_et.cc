0ae7b6f589a6c7300d2f79c15612b375b7dd0495
Carl Timmer <timmer@jlab.org>
2023-04-26 10:57:27 -0400

    add cmd line arg to set incoming data source ids

--- simulation/udp_rcv_et.cc
+++ simulation/udp_rcv_et.cc
@@ -9,10 +9,28 @@
 
 
 /**
- * @file Receive locally generated data sent by udp_send.c program.
+ * @file
+ * Receive data sent by source ids given on command line.
+ * Place those in an ET system acting as a fifo.
+ * You'll have to coordinate the number of data sources, with the setting up of the ET system.
+ * For example, for 3 sources, run the ET with something like:
+ *
+ * <code>
+ *   et_start_fifo -f /tmp/fifoEt -d -s 150000 -n 3 -e 1000
+ * </code>
+ *
+ * You can then run this program like:
+ *
+ * <code>
+ *   udp_rcv_et -et /tmp/fifoEt -ids 1,3,76 -p 17750
+ * </code>
+ *
+ * This expects data sources 1,3, and 76. There will be room in each ET fifo entry to have
+ * 3 buffers (ET events), one for each source. There will be 1000 entries. Each buffer will
+ * be 150kB. Max # of sources is 16 (can change that below).
  */
 
-#include <string.h>
+#include <iostream>
 #include "ejfat_assemble_ersap_et.hpp"
 
 using namespace ejfat;
@@ -22,6 +40,8 @@ using namespace ejfat;
 // Be sure to print to stderr as this program pipes data to stdout!!!
 //-----------------------------------------------------------------------
 
+// Max # of data input sources
+#define MAX_SOURCES 16
 
 #define INPUT_LENGTH_MAX 256
 
@@ -32,11 +52,12 @@ using namespace ejfat;
  */
 static void printHelp(char *programName) {
     fprintf(stderr,
-            "\nusage: %s\n%s\n%s\n%s\n%s\n\n",
+            "\nusage: %s\n%s\n%s\n%s\n%s\n%s\n\n",
             programName,
             "        [-h] [-v] ",
             "        [-a <listening IP address (defaults to INADDR_ANY)>]",
             "        [-p <listening UDP port>]",
+            "        [-ids <comma-separated list of incoming source ids>]",
             "        [-et <ET file name>]");
 
     fprintf(stderr, "        This is an EJFAT UDP packet receiver.\n");
@@ -53,7 +74,7 @@ static void printHelp(char *programName) {
  * @param fileName    filled with output file name.
  * @param listenAddr  filled with IP address to listen on.
  */
-static void parseArgs(int argc, char **argv, uint16_t* port, bool *debug,
+static void parseArgs(int argc, char **argv, uint16_t* port, bool *debug, int *sourceIds,
                       char *etFileName, char *listenAddr) {
 
     int c, i_tmp;
@@ -63,6 +84,7 @@ static void parseArgs(int argc, char **argv, uint16_t* port, bool *debug,
     static struct option long_options[] =
             {
                     {"et", 1, NULL, 1},
+                    {"ids",  1, NULL, 2},
                     {0,    0, 0,    0}
             };
 
@@ -80,6 +102,65 @@ static void parseArgs(int argc, char **argv, uint16_t* port, bool *debug,
                     exit(-1);
                 }
                 strcpy(etFileName, optarg);
+                break;
+
+            case 2:
+                // Incoming source ids
+                if (strlen(optarg) < 1) {
+                    fprintf(stderr, "Invalid argument to -ids, need comma-separated list of ids\n");
+                    exit(-1);
+                }
+
+
+                {
+                    // split into ints
+                    std::string s = optarg;
+                    std::string delimiter = ",";
+
+                    size_t pos = 0;
+                    std::string token;
+                    char *endptr;
+                    int index = 0;
+                    bool oneMore = true;
+
+                    while ((pos = s.find(delimiter)) != std::string::npos) {
+                        //fprintf(stderr, "pos = %llu\n", pos);
+                        token = s.substr(0, pos);
+                        errno = 0;
+                        sourceIds[index] = (int) strtol(token.c_str(), &endptr, 0);
+
+                        if ((token.c_str() - endptr) == 0) {
+                            //fprintf(stderr, "two commas next to eachother\n");
+                            oneMore = false;
+                            break;
+                        }
+                        index++;
+                        //std::cout << token << std::endl;
+                        s.erase(0, pos + delimiter.length());
+                        if (s.length() == 0) {
+                            //fprintf(stderr, "break on zero len string\n");
+                            oneMore = false;
+                            break;
+                        }
+                    }
+
+                    if (oneMore) {
+                        errno = 0;
+                        sourceIds[index] = (int) strtol(s.c_str(), nullptr, 0);
+                        if (errno == EINVAL || errno == ERANGE) {
+                            fprintf(stderr, "Invalid argument to -ids, need comma-separated list of ids\n");
+                            exit(-1);
+                        }
+                        index++;
+                        //std::cout << s << std::endl;
+                    }
+
+                    if (index > MAX_SOURCES) {
+                        fprintf(stderr, "Too many sources specified in -ids, max %d\n", MAX_SOURCES);
+                        exit(-1);
+                    }
+                }
+
                 break;
 
             case 'p':
@@ -138,11 +219,34 @@ int main(int argc, char **argv) {
     uint16_t port = 7777;
     bool debug = true;
 
+    int sourceIds[MAX_SOURCES];
+    int sourceCount = 0;
+
     char etFileName[INPUT_LENGTH_MAX], listeningAddr[16];
     memset(etFileName, 0, INPUT_LENGTH_MAX);
     memset(listeningAddr, 0, 16);
 
-    parseArgs(argc, argv, &port, &debug, etFileName, listeningAddr);
+    for (int i = 0; i < MAX_SOURCES; i++) {
+        sourceIds[i] = -1;
+    }
+
+    parseArgs(argc, argv, &port, &debug, sourceIds, etFileName, listeningAddr);
+
+    for (int i = 0; i < MAX_SOURCES; i++) {
+        if (sourceIds[i] > -1) {
+            sourceCount++;
+            std::cerr << "Expecting source " << sourceIds[i] << " in position " << i << std::endl;
+        }
+        else {
+            break;
+        }
+    }
+
+    if (sourceCount < 1) {
+        sourceIds[0] = 0;
+        sourceCount = 1;
+        std::cerr << "Defaulting to (single) source id = 0" << std::endl;
+    }
 
     // Create UDP socket
     udpSocket = socket(AF_INET, SOCK_DGRAM, 0);
@@ -179,24 +283,8 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    // Tell ET-based FIFO what source ids are expected
-    //    int idCount = 3;
-    //    int bufIds[idCount];
-    //    bufIds[0]=1;
-    //    bufIds[1]=2;
-    //    bufIds[2]=77;
-
-        int idCount = 2;
-        int bufIds[idCount];
-        bufIds[0]=1;
-        bufIds[1]=2;
-
-//    int idCount = 1;
-//    int bufIds[idCount];
-//    bufIds[0]=1;
-
     // We're a producer of FIFO data
-    err = et_fifo_openProducer(etid, &fid, bufIds, idCount);
+    err = et_fifo_openProducer(etid, &fid, sourceIds, sourceCount);
     if (err != ET_OK) {
         et_perror(err);
         exit(1);
