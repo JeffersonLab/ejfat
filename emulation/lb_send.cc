fecff1a7d795778c3a7815f8d63873c9871372b0
Carl Timmer <timmer@jlab.org>
2023-05-04 15:11:21 -0400

    use new RE header

--- emulation/lb_send.cc
+++ emulation/lb_send.cc
@@ -1,4 +1,4 @@
-/************* Loab Balancer Send  *******************/
+/************* Load Balancer Send  *******************/
 #include <unistd.h>
 
 #include <fstream>
@@ -10,10 +10,10 @@ void   Usage(void)
 {
     char usage_str[] =
         "\nUsage: \n\
-        -s seq #  \n\
+        -e event #  \n\
         -h help \n\n";
         cout<<usage_str;
-        cout<<"Required: -s\n";
+        cout<<"Required: -e\n";
 }
 
 int main (int argc, char *argv[])
@@ -22,19 +22,19 @@ int main (int argc, char *argv[])
     extern char *optarg;
     extern int   optind, optopt;
 
-    unsigned int seq;
-    bool passedS = false;
+    uint32_t event_num;
+    bool passedE = false;
 
-    while ((optc = getopt(argc, argv, "s:")) != -1)
+    while ((optc = getopt(argc, argv, "e:")) != -1)
     {
         switch (optc)
         {
         case 'h':
             Usage();
             exit(1);
-        case 's':
-            seq = atoi((const char *) optarg) ;
-            passedS = true;
+        case 'e':
+            event_num = atoi((const char *) optarg) ;
+            passedE = true;
             break;
         case '?':
             cerr<<"Unrecognised option: -"<<optopt<<'\n';
@@ -43,47 +43,52 @@ int main (int argc, char *argv[])
         }
     }
 
-    if(!passedS) { Usage(); exit(1); }
+    if(!passedE) { Usage(); exit(1); }
 
     ifstream f1("/dev/stdin", std::ios::binary | std::ios::in);
     ofstream f2("/dev/stdout",std::ios::binary | std::ios::out);
 
     // prepare LB meta-data
-    // LB meta-data header on front of payload
+    // New LB meta-data header on front of payload
     union {
         struct __attribute__((packed)) lb_hdr {
-            unsigned int l    : 8;
-            unsigned int b    : 8;
-            unsigned int vrsn : 8;
-            unsigned int ptcl : 8;
-            unsigned long int tick : 64;
+            uint32_t l      : 8;
+            uint32_t b      : 8;
+            uint32_t vrsn   : 8;
+            uint32_t ptcl   : 8;
+            uint32_t rsrvd  : 16;
+            uint32_t ent    : 16;
+            uint64_t event  : 64;
         } lbmdbf;
-        unsigned int lbmduia [3];
+        unsigned int lbmduia [4];
     } lbmd;
 
-    lbmd.lbmdbf = {'L','B',1,1,1};
+    lbmd.lbmdbf = {'L','B',2,1,0, 0, 1};
+    lbmd.lbmdbf.event = event_num; //passed in
 
-    // prepare RE meta-data
-    // RE meta-data header on front of payload
+    // New RE meta-data header on front of payload
     union {
         struct __attribute__((packed))re_hdr {
-            unsigned int vrsn    : 4;
-            unsigned int rsrvd   : 10;
-            unsigned int frst    : 1;
-            unsigned int lst     : 1;
-            unsigned int data_id : 16;
-            unsigned int seq     : 32;
+            uint32_t vrsn    : 4;
+            uint32_t rsrvd   : 12;
+            uint32_t data_id : 16;
+            uint32_t offset  : 32;
+            uint32_t length  : 32;
+            uint64_t event   : 64;
         } remdbf;
-        unsigned int remduia[2];
+        uint32_t remduia[5];
     } remd;
 
-    remd.remdbf = {1,0,1,0,1,1};
-    remd.remdbf.seq = seq; //passed in
+    remd.remdbf = {2,0,1,0,1024,1};
+    remd.remdbf.event = event_num; //passed in
 
-    f2.write((char*)lbmd.lbmduia, 3*sizeof(lbmd.lbmduia[0]));
-    f2.write((char*)remd.remduia, 2*sizeof(remd.remduia[0]));
     char x[1024]; //packet size limit
+
     f1.read((char*)(&x), sizeof(x));
+
+    remd.remdbf.length = f1.gcount();
+    f2.write((char*)lbmd.lbmduia, 4*sizeof(lbmd.lbmduia[0]));
+    f2.write((char*)remd.remduia, 5*sizeof(remd.remduia[0]));
     f2.write((char*)x, f1.gcount());
 
     return 0;
--- emulation/pktzr_md.cc
+++ emulation/pktzr_md.cc
@@ -1,7 +1,7 @@
-/************* Loab Balancer Packetizer / Sender  *******************/
+/************* Load Balancer Packetizer / Sender  *******************/
 #include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -9,18 +9,20 @@
 #include <netinet/udp.h>
 #include <net/if.h>
 #include <arpa/inet.h>
-#include <string.h>
+#include <cstring>
 #include <fstream>
 #include <iostream>
-#include <inttypes.h>
-#include <time.h>
+#include <cinttypes>
+#include <ctime>
 
 using namespace std;
 
-#define HTONLL(x) ((1==htonl(1)) ? (x) : (((uint64_t)htonl((x) & 0xFFFFFFFFUL)) << 32) | htonl((uint32_t)((x) >> 32)))
-#define NTOHLL(x) ((1==ntohl(1)) ? (x) : (((uint64_t)ntohl((x) & 0xFFFFFFFFUL)) << 32) | ntohl((uint32_t)((x) >> 32)))
+#ifdef __linux__
+    #define HTONLL(x) ((1==htonl(1)) ? (x) : (((uint64_t)htonl((x) & 0xFFFFFFFFUL)) << 32) | htonl((uint32_t)((x) >> 32)))
+    #define NTOHLL(x) ((1==ntohl(1)) ? (x) : (((uint64_t)ntohl((x) & 0xFFFFFFFFUL)) << 32) | ntohl((uint32_t)((x) >> 32)))
+#endif
 
-void   Usage(void)
+void   Usage()
 {
     char usage_str[] =
         "\nUsage: \n\
@@ -29,7 +31,7 @@ void   Usage(void)
         -p destination port (number)  \n\
         -t lb_tick  \n\
         -d re_data_id  \n\
-        -e Use LB entropy  \n\
+        -e re_entropy  \n\
         -v verbose mode (default is quiet)  \n\
         -s max packet size (default 9000)  \n\
         -h help \n\n";
@@ -41,13 +43,10 @@ int main (int argc, char *argv[])
 {
     const size_t max_pckt_sz = 9000-20-8;  // = MTU - IP header - UDP header
     const size_t lblen       = 16;
-	const size_t enet_pad    = 2;
-	const size_t relen       = 8+8+enet_pad;     // 8 for flags, data_id, 8 for tick (event_id), 2bytes of pad to avoid ethernet packets < 64B
+	const size_t relen       = 20;     // 4 for ver & data_id, 4 for offset, 4 for len, 8 for tick (event_id)
     const size_t mdlen       = lblen + relen;
 
     int optc;
-    extern char *optarg;
-    extern int   optind, optopt;
 
     bool passedI=false, passedP=false, passed6=false, passedV=false, passedE=false;
 
@@ -57,17 +56,31 @@ int main (int argc, char *argv[])
     const uint8_t lb_vrsn    = 2;
     const uint8_t lb_prtcl   = 1;
     const uint16_t lb_rsrvd  = 0;
+
     uint64_t lb_tick         = 1;      // LB tick
-    const uint8_t re_vrsn    = 1;
+    uint64_t lb_tick_net;              // LB tick in network byte order
+
+    const uint8_t re_vrsn    = 2;
     const uint16_t re_rsrvd  = 0;
+
     uint8_t re_frst          = 1;
-    uint8_t re_lst           = 0;
+    uint8_t re_lst           = 0;      // no longer in RE header, but useful in program
+
     uint16_t re_data_id      = 1;      // RE data_id
-    uint32_t re_seq          = 0;
+    uint16_t re_data_id_net;           // RE data_id in network byte order
+
+    uint16_t re_entropy      = 0;      // RE entropy
+    uint16_t re_entropy_net;           // RE entropy in network byte order
+
+    uint32_t re_off          = 0;      // RE offset into reassembled buf
+    uint32_t re_off_net;               // RE offset in network byte order
+
+    uint32_t re_len          = 0;      // RE byte len of reassembled buf
+    uint32_t re_len_net;               // RE byte len in network byte order
 
     size_t pckt_sz = max_pckt_sz;
 
-    while ((optc = getopt(argc, argv, "i:p:t:d:n:e6vs:")) != -1)
+    while ((optc = getopt(argc, argv, "i:p:t:d:n:e:6vs:")) != -1)
     {
         switch (optc)
         {
@@ -90,11 +103,11 @@ int main (int argc, char *argv[])
             break;
         case 't':
             lb_tick = (uint64_t) atoi((const char *) optarg) ;
-            fprintf(stdout, "-t %lu", lb_tick);
+            fprintf(stdout, "-t %" PRIu64 "", lb_tick);
             break;
         case 'd':
             re_data_id = (uint16_t) atoi((const char *) optarg) ;
-            fprintf(stdout, "-d %d ", re_data_id);
+            fprintf(stdout, "-d %hu ", re_data_id);
             break;
         case 's':
             pckt_sz = (size_t) atoi((const char *) optarg) -20-8;  // = MTU - IP header - UDP header
@@ -103,7 +116,8 @@ int main (int argc, char *argv[])
             break;
         case 'e':
             passedE = true;
-            fprintf(stdout, "-e ");
+            re_entropy = (uint16_t) atoi((const char *) optarg) ;
+            fprintf(stdout, "-e %hu", re_entropy);
             break;
         case 'v':
             passedV = true;
@@ -170,79 +184,121 @@ int main (int argc, char *argv[])
 //=======================================================================
     inet_pton(AF_INET6, dst_ip, &dst_addr6.sin6_addr);  // LB address
 
-    uint8_t buffer[pckt_sz];
+    uint8_t header_local[mdlen];  // LB and RE headers in local byte order
+    uint8_t buffer_net[pckt_sz];  // network byte order data to send
 
     // meta-data in network order
 	// LB metadata
-    uint8_t*  pBufLb =  buffer;
+    uint8_t*  pBufLb = buffer_net;
     pBufLb[0] = 'L'; // 0x4c
     pBufLb[1] = 'B'; //0x42
     pBufLb[2] = lb_vrsn;   //version
     pBufLb[3] = lb_prtcl;  //protocol
     pBufLb[4] = 0;   //reserved
     pBufLb[5] = 0;   //reserved
-    uint16_t* pEntrp  = (uint16_t*) &pBufLb[6];
-    uint64_t* pTick   = (uint64_t*) &pBufLb[8];
-    *pTick    = HTONLL(lb_tick);
-	// RE metadata
-    uint8_t*  pBufRe = &buffer[lblen];
-    pBufRe[0] = 0x10; //(re_vrsn & 0xf) + (re_rsrvd & 0x3f0) >> 4;
-    pBufRe[1] = 0x2; //(re_rsrvd  & 0x3f) << 2 + (re_frst << 1) + re_lst;
-    uint16_t* pDid    = (uint16_t*) &pBufRe[2];
-    uint32_t* pSeq    = (uint32_t*) &pBufRe[4];
-    uint64_t* pReTick = (uint64_t*) &pBufRe[8];
-    *pDid     = htons(re_data_id);
-    *pSeq     = htonl(re_seq);
-    *pReTick  = *pTick;
+    auto* pEntrp  = (uint16_t*) &pBufLb[6];
+    auto* pTick   = (uint64_t*) &pBufLb[8];
+
+    // For verbose output, keep track of local byte order LB header
+    if (passedV) {
+        memcpy(header_local, pBufLb, lblen);
+        auto* pEnt  = (uint16_t*) &header_local[6];
+        auto* pTik  = (uint64_t*) &header_local[8];
+        *pEnt       = re_entropy;
+        *pTik       = lb_tick;
+    }
+
+    // put LB header in network byte order
+    *pEntrp     = re_entropy_net = htons(re_entropy); // htons does NOT swap the arg iteself!
+    lb_tick_net = lb_tick;
+    *pTick      = HTONLL(lb_tick_net);                // HTONLL swaps the arg itself!
+    
+
+    // RE metadata
+    uint8_t*  pBufRe = &buffer_net[lblen];
+    pBufRe[0] = re_vrsn << 4;
+    pBufRe[1] = 0;
+    auto* pDid    = (uint16_t*) &pBufRe[2];
+    auto* pOff    = (uint32_t*) &pBufRe[4];
+    auto* pLen    = (uint32_t*) &pBufRe[8];
+    auto* pReTick = (uint64_t*) &pBufRe[12];
+
+    // For verbose output, keep track of local byte order RE header
+    if (passedV) {
+        memcpy(header_local+lblen, pBufRe, 2);
+        auto* pD = (uint16_t*) &header_local[2 + lblen];
+        auto* pO = (uint64_t*) &header_local[4 + lblen];
+        auto* pL = (uint32_t*) &header_local[8 + lblen];
+        auto* pR = (uint64_t*) &header_local[12 + lblen];
+
+        *pD = re_data_id;
+        *pO = re_off;
+        *pL = re_len;
+        *pR = lb_tick;
+    }
+
+    // put in network byte order
+    *pDid          = re_data_id_net = htons(re_data_id); // does NOT swap arg itself
+    *pOff          = re_off_net = htonl(re_off);
+    *pReTick       = *pTick; // already swapped above
+
+    // Find out how many bytes we'll be sending.
+    // Go to end of input
+    f1.seekg(0, ios_base::end);
+    // Read how many preceding bytes in input
+    re_len = (int) f1.tellg();
+    *pLen = re_len_net = htonl(re_len);
+    // Go back to the beginning for reading input
+    f1.seekg(0, ios_base::beg);
+
+    std::cout  << "\nLook like stdin is of len " << re_len << " bytes" << std::endl;
 
     do {
-        f1.read((char*)&buffer[mdlen], num_to_read);
+        f1.read((char*)&buffer_net[mdlen], num_to_read);
         streamsize nr = f1.gcount();
         if(passedV) cout  << "\nNum read from stdin: " << nr << endl;
         if(nr != num_to_read) {
-            re_lst  = 1;
-            pBufRe[1] = 0x1; //(re_rsrvd  & 0x3f) << 2 + (re_frst << 1) + re_lst;
+            // last read from file piped in thru stdin
+            re_lst = 1;
         }
 
         // forward data to LB
 
-        if(passedE) *pEntrp = htons(re_data_id); else *pEntrp = 0;
-
         if(passedV) {
             fprintf ( stdout, "\nLB Meta-data:\n");
-            for(uint8_t b = 0; b < lblen; b++) fprintf ( stdout, " [%d] = %x", b, pBufLb[b]);
-            fprintf ( stdout, " entropy = %d", ntohs(*pEntrp));
+            for(uint8_t b = 0; b < lblen; b++) fprintf ( stdout, " [%d] = %x", b, header_local[b]);
+            fprintf ( stdout, " entropy = %hu", re_entropy);
             fprintf ( stdout, " lb_tick = %" PRIu64 "\n", lb_tick);
 
             fprintf ( stdout, "\nLB Meta-data on the wire:\n");
             for(uint8_t b = 0; b < lblen; b++) fprintf ( stdout, " [%d] = %x", b, pBufLb[b]);
-            fprintf ( stdout, " entropy = %d", (*pEntrp));
-            fprintf ( stdout, " for lb_tick = %" PRIu64 " ", *pTick);
+            fprintf ( stdout, " entropy = %hu", re_entropy_net);
+            fprintf ( stdout, " for lb_tick = %" PRIu64 "\n", lb_tick_net);
 
             fprintf ( stdout, "\nRE Meta-data:\n");
-            for(uint8_t b = 0; b < relen; b++) fprintf ( stdout, " [%d] = %x ", b, pBufRe[b]);
-            fprintf ( stdout, " for re_frst = %d / re_lst = %d", re_frst, re_lst); 
-            fprintf ( stdout, " / re_data_id = %d / re_seq = %d\n", re_data_id, re_seq);	
-            fprintf ( stdout, " re_tick = %" PRIu64 "\n", lb_tick);
+            for(uint8_t b = 0; b < relen; b++) fprintf ( stdout, " [%d] = %x ", b, header_local[b + lblen]);
+            fprintf ( stdout, "\n for re_frst = %d / re_lst = %d", re_frst, re_lst);
+            fprintf ( stdout, " / re_data_id %hu / re_off %u / re_len %u / re_tick %" PRIu64 "\n",
+                      re_data_id, re_off, re_len, lb_tick);
 
             fprintf ( stdout, "\nRE Meta-data on the wire:\n");
             for(uint8_t b = 0; b < relen; b++) fprintf ( stdout, " [%d] = %x ", b, pBufRe[b]);
-            fprintf ( stdout, " for re_frst = %d / re_lst = %d", re_frst, re_lst); 
-            fprintf ( stdout, " / re_data_id = %d / re_seq = %d\n", *pDid, *pSeq);	
-            fprintf ( stdout, " for re_tick = %" PRIu64 " ", *pReTick);
+            fprintf ( stdout, "\n for re_frst = %d / re_lst = %d", re_frst, re_lst);
+            fprintf ( stdout, " / re_data_id %hu / re_off %u / re_len %u / re_tick %" PRIu64 "\n",
+                      re_data_id_net, re_off_net, re_len_net, lb_tick_net);
         }
 
         ssize_t rtCd = 0;
         /* now send a datagram */
         size_t num_to_send = nr+mdlen; // max bytes to send including metadata
         if (passed6) {
-            if ((rtCd = sendto(dst_sckt, buffer, num_to_send, 0, 
+            if ((rtCd = sendto(dst_sckt, buffer_net, num_to_send, 0,
                     (struct sockaddr *)&dst_addr6, sizeof dst_addr6)) < 0) {
                 perror("sendto failed");
                 exit(4);
             }
         } else {
-            if ((rtCd = sendto(dst_sckt, buffer, num_to_send, 0, 
+            if ((rtCd = sendto(dst_sckt, buffer_net, num_to_send, 0,
                     (struct sockaddr *)&dst_addr, sizeof dst_addr)) < 0) {
                 perror("sendto failed");
                 exit(4);
@@ -250,17 +306,22 @@ int main (int argc, char *argv[])
         }
         if(passedV) fprintf ( stdout, "\nSending %d bytes to %s : %u\n", uint16_t(rtCd), dst_ip, dst_prt);
         re_frst = 0;
-        pBufRe[1] = 0x0; //(re_rsrvd  & 0x3f) << 2 + (re_frst << 1) + re_lst;
-        *pSeq = htonl(++re_seq);
+
+        re_off += nr;
+        *pOff = re_off_net = htonl(re_off);
+
     } while(!re_lst);
-            // `time_t` is an arithmetic time type
-            time_t now;
-         
-            // Obtain current time
-            // `time()` returns the current time of the system as a `time_t` value
-            time(&now);
-         
-            // Convert to local time format and print to stdout
-            fprintf ( stdout, "Today is %s", ctime(&now));
+
+
+    // `time_t` is an arithmetic time type
+    time_t now;
+
+    // Obtain current time
+    // `time()` returns the current time of the system as a `time_t` value
+    time(&now);
+
+    // Convert to local time format and print to stdout
+    fprintf ( stdout, "Today is %s", ctime(&now));
+
     return 0;
 }
