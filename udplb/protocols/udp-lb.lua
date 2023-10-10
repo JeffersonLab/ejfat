b27970e65dc0b364de4ed4018e763172489492c3
Michael S. Goodrich <goodrich@jlab.org>
2023-10-10 11:13:07 -0400

    Update wireshark/tshark LB metadata dissector

--- udplb/protocols/udp-lb.lua
+++ udplb/protocols/udp-lb.lua
@@ -1,15 +1,19 @@
 --
 local p_udplb = Proto("udplb", "UDP Load Balancer Protocol")
 
-local f_magic = ProtoField.string("udplb.magic", "Magic", base.ASCII)
-local f_version = ProtoField.uint8("udplb.version", "Version", base.DEC)
-local f_proto = ProtoField.uint8("udplb.proto", "Protocol", base.HEX)
-local f_tick = ProtoField.uint64("udplb.tick", "Tick", base.HEX)
+local f_magic   = ProtoField.string("udplb.magic",   "Magic",    base.ASCII)
+local f_version = ProtoField.uint8( "udplb.version", "Version",  base.DEC)
+local f_proto   = ProtoField.uint8( "udplb.proto",   "Protocol", base.HEX)
+local f_rsrvd   = ProtoField.uint16("udplb.rsrvd",   "Reserved", base.HEX)
+local f_entropy = ProtoField.uint16("udplb.entropy", "Entropy",  base.HEX)
+local f_tick    = ProtoField.uint64("udplb.tick",    "Tick",     base.HEX)
 
 p_udplb.fields = {
    f_magic,
    f_version,
    f_proto,
+   f_rsrvd,
+   f_entropy,
    f_tick,
 }
 
@@ -19,18 +23,20 @@ local proto = Field.new("udplb.proto")
 local tick = Field.new("udplb.tick")
 
 function p_udplb.dissector(buf, pkt, tree)
-   local t = tree:add(p_udplb, buf(0, 12))
-   t:add(f_magic, buf(0,2))
+   local t = tree:add(p_udplb, buf(0, 16))
+   t:add(f_magic,   buf(0,2))
    t:add(f_version, buf(2,1))
-   t:add(f_proto, buf(3,1))
-   t:add(f_tick, buf(4,8))
+   t:add(f_proto,   buf(3,1))
+   t:add(f_rsrvd,   buf(4,2))
+   t:add(f_entropy, buf(6,2))
+   t:add(f_tick,    buf(8,8))
 
    -- local proto = buf(13,1):uint()
    local dissector = p_udplb_encap_table:get_dissector(proto()())
 
    if dissector ~= nil then
       -- found a dissector
-      dissector:call(buf(12):tvb(), pkt, tree)
+      dissector:call(buf(16):tvb(), pkt, tree)
    else
       pkt.cols.protocol:set("UDP-LB")
       pkt.cols.packet_len:set(buf:len())
