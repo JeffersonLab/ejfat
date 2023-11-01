7126f4867e4efcd0b65067d00154395f936cb298
Carl Timmer <carl.timmer.1@gmail.com>
2023-11-01 19:01:39 -0400

    add header that includes all other headers for utils

---
+++ util/ejfatUtil.h
@@ -0,0 +1,28 @@
+//
+// Copyright 2023, Jefferson Science Associates, LLC.
+// Subject to the terms in the LICENSE file found in the top-level directory.
+//
+// EPSCI Group
+// Thomas Jefferson National Accelerator Facility
+// 12000, Jefferson Ave, Newport News, VA 23606
+// (757)-269-7100
+
+#ifndef EJFAT_EJFATUTIL_H
+#define EJFAT_EJFATUTIL_H
+
+
+#include "BufferItem.h"
+#include "BufferSupply.h"
+#include "BufferSupplyItem.h"
+#include "ByteBuffer.h"
+#include "ByteOrder.h"
+#include "PacketRefItem.h"
+#include "PacketsItem.h"
+#include "PacketsItemN.h"
+#include "PacketStoreItem.h"
+#include "Supplier.h"
+#include "SupplierN.h"
+#include "SupplyItem.h"
+
+
+#endif //EJFAT_EJFATUTIL_H
