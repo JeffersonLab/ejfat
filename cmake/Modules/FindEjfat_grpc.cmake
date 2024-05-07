3a1cd72bd0f7dfc389caea83aec5718e3101d0dd
Carl Timmer <carl.timmer.1@gmail.com>
2024-05-07 09:36:29 -0400

    fix bugs causing cmake to choke on finding ejfat_grpc lib

--- cmake/Modules/FindEjfat_grpc.cmake
+++ cmake/Modules/FindEjfat_grpc.cmake
@@ -11,11 +11,11 @@ pkg_check_modules(PC_LIBGRPC QUIET ejfat_grpc)
 set(GRPC_VERSION 1.0)
 
 find_path(Ejfat_grpc_INCLUDE_DIR lb_cplane.h
-        PATHS ${INSTALL_DIR}/include $ENV{GRPC_INSTALL_DIR}/include)
+        PATHS ${INSTALL_DIR}/include $ENV{GRPC_INSTALL_DIR}/include ${EJFAT_ERSAP_INSTALL_DIR}/include)
 
 find_library(Ejfat_grpc_LIBRARY
         NAMES ejfat_grpc
-        PATHS ${INSTALL_DIR}/lib $ENV{GRPC_INSTALL_DIR}/lib)
+        PATHS ${INSTALL_DIR}/lib $ENV{GRPC_INSTALL_DIR}/lib ${EJFAT_ERSAP_INSTALL_DIR}/lib)
 
 if(Ejfat_grpc_LIBRARY)
     set(Ejfat_grpc_FOUND ON)
@@ -27,12 +27,12 @@ set(Ejfat_grpc_INCLUDE_DIRS ${Ejfat_grpc_INCLUDE_DIR})
 message(STATUS "Ejfat_grpc_LIBRARIES = " ${Ejfat_grpc_LIBRARIES})
 message(STATUS "Ejfat_grpc_INCLUDE_DIRS = " ${Ejfat_grpc_INCLUDE_DIRS})
 
-if(NOT TARGET libejfatGrpcLib)
+if(NOT TARGET ejfat_grpc)
     add_library(ejfat_grpc UNKNOWN IMPORTED)
 
     set_target_properties(ejfat_grpc PROPERTIES
-            IMPORTED_LOCATION ${GRPC_LIBRARY}
-            INTERFACE_INCLUDE_DIRECTORIES ${GRPC_INCLUDE_DIRS})
+            IMPORTED_LOCATION ${Ejfat_grpc_LIBRARY}
+            INTERFACE_INCLUDE_DIRECTORIES ${Ejfat_grpc_INCLUDE_DIRS})
 endif()
 
 include (FindPackageHandleStandardArgs )
