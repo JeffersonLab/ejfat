aa52ffac91adce6097c1922c7f8d49fa0cfd17dd
gurjyan <gurjyan@jlab.org>
2024-05-31 14:59:58 -0400

    Debugging: Add new metrics to Prometheus extractor, such as metrics used for PID control

--- simulation/packetBlasteeEtMTPRT.cc
+++ simulation/packetBlasteeEtMTPRT.cc
@@ -696,6 +696,8 @@ typedef struct threadStruct_t {
 
 // Thread to monitor the ET system, run PID loop and report back to control plane
 static void *pidThread(void *arg) {
+
+  printf("DDDDDDD \n");
   // Initialize Prometheus metrics
   auto& fillPercentP = ejfat_be.Add({{"fillPercent", "value"}});
   auto& fillAvgP = ejfat_be.Add({{"fillAvg", "value"}});
